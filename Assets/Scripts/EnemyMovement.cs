54e8fe77ff15c75064bdd76b409fb24af75571d0
GalinSavov <lemonkiller34@gmail.com>
2024-06-16 11:55:24 +0200

    Added enemies and enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Enemies
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb = null;
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private BoxCollider2D boxCollider = null;
+        void Update()
+        {
+            
+            Move();
+        }
+        private void Move()
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            moveSpeed = -moveSpeed;
+            transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+        }
+    }
+}
