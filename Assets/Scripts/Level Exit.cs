9eceb3c3e94826ae1c83c56c1115540d205e6db5
GalinSavov <lemonkiller34@gmail.com>
2024-06-18 12:04:26 +0200

    Added scene transitions

---
+++ Assets/Scripts/Level Exit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Interactables
+{
+    public class LevelExit : MonoBehaviour
+    {
+        
+        private IEnumerator LoalNextLevel()
+        {
+         
+            yield return new WaitForSeconds(1.5f);
+            if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+            {
+                //load the main menu in the future
+                SceneManager.LoadScene(0);
+            }
+            else if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            StartCoroutine(LoalNextLevel());
+        }
+    }
+}
