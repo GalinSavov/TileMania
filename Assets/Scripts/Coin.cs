dc6178c2e4c62a1eef2218b3d336d9c0d0383886
GalinSavov <lemonkiller34@gmail.com>
2024-06-18 15:38:36 +0200

    Added prefab variants

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -8,10 +8,13 @@ namespace Game.Interactables
     {
         [SerializeField] private AudioSource audioSource = null;
         [SerializeField] private int pointsAwardedOnPickup = 100;
+
+        private bool hasPickedUp = true;
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.CompareTag("Player"))
+            if(collision.CompareTag("Player") && hasPickedUp)
             {
+                hasPickedUp = false;
                 GameManager.Instance.IncreaseScore(pointsAwardedOnPickup);
                 AudioSource.PlayClipAtPoint(audioSource.clip, Camera.main.transform.position); 
                 Destroy(gameObject);
