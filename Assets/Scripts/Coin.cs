fb6f9177475ae10c99e58f2749467ee5b7f3bc0d
GalinSavov <lemonkiller34@gmail.com>
2024-06-18 14:03:35 +0200

    Added a coin pickup sound effect

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,13 +6,16 @@ namespace Game.Interactables
 {
     public class Coin : MonoBehaviour
     {
+        [SerializeField] private AudioSource audioSource;
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.gameObject.tag == "Player")
+            if(collision.CompareTag("Player"))
             {
+                AudioSource.PlayClipAtPoint(audioSource.clip, Camera.main.transform.position); 
                 Destroy(gameObject);
             }
         }
+     
     }
 
 }
